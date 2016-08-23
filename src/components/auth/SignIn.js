505aea6e845995fed203b881ecb8a2e590a23b45
Emma J <codemzy@gmail.com>
2016-08-23 15:53:05 +0000

    Wired up reduc form with the SignIn component form elements

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { reduxForm } from 'redux-form';
 
 class SignIn extends React.Component {
+    
+    _handleFormSubmit({ email, password }) {
+        console.log(email, password);
+    }
+    
     render() {
+        const { handleSubmit, fields: { email, password }} = this.props;
         return (
-            <form>
+            <form onSubmit={handleSubmit(this._handleFormSubmit.bind(this))}>
                 <fieldset className="form-group">
                     <label>Email:</label>
-                    <input className="form-control" />
+                    <input type="text" className="form-control" {...email} />
                 </fieldset>
                 <fieldset className="form-group">
                     <label>Password:</label>
-                    <input className="form-control" />
+                    <input type="password" className="form-control" {...password} />
                 </fieldset>
                 <button action="submit" className="btn btn-primary">Sign In</button>
             </form>
