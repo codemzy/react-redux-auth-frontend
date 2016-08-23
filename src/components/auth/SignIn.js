6e1c02337aff6eab226c5fcc87f612b496aca2da
Emma J <codemzy@gmail.com>
2016-08-23 15:48:47 +0000

    Added form to SignIn component

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -4,7 +4,17 @@ import { reduxForm } from 'redux-form';
 class SignIn extends React.Component {
     render() {
         return (
-            
+            <form>
+                <fieldset className="form-group">
+                    <label>Email:</label>
+                    <input className="form-control" />
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Password:</label>
+                    <input className="form-control" />
+                </fieldset>
+                <button action="submit" className="btn btn-primary">Sign In</button>
+            </form>
         );
     }
 }
