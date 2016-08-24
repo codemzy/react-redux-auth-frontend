2187460c7b8005c923916ae09a363e41c4c7e5a0
Emma J <codemzy@gmail.com>
2016-08-24 14:03:20 +0000

    Added Register component and wired up to router

---
+++ src/components/auth/Register.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { reduxForm } from 'redux-form'
+
+// actions
+// TODO
+
+class Register extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Register</h1>
+            </div>
+        );
+    }
+}
+
+export default Register;
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import SignIn from './components/auth/SignIn';
 import SignOut from './components/auth/SignOut';
-
+import Register from './components/auth/Register';
 
 export default (
     <Route path='/' component={App}>
         <Route path="/signin" component={SignIn} />
         <Route path="/signout" component={SignOut} />
+        <Route path="/register" component={Register} />
     </Route>
 );
