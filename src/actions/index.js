d32f05938107a037c68dc4bab41b8660c98a6eda
Emma J <codemzy@gmail.com>
2016-08-24 12:07:56 +0000

    Updated auth reducer to handle AUTH_USER and UNAUTH_USER action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 
+// action types
+import { AUTH_USER, UNAUTH_USER } from './types';
+
 const ROOT_URL = 'https://node-jwt-auth-api-codemzy.c9users.io';
 
 export function signinUser({email, password}) {
@@ -10,7 +13,8 @@ export function signinUser({email, password}) {
         // submit email and password to the api server
         axios.post(ROOT_URL + '/signin', { email, password })
             .then((response) => {
-                // if request is good update state to indicate user is authenticated
+                // if request is good update state to indicate user is authenticated (dispatch action)
+                dispatch({ type: AUTH_USER });
                 // save JWT token
                 // redirect to the route '/feature' using react router
                 browserHistory.push('/feature');
--- src/reducers/reducer_auth.js
+++ src/reducers/reducer_auth.js
@@ -1,6 +1,17 @@
+import { AUTH_USER, UNAUTH_USER } from '../actions/types';
+
 export default function(state = {}, action) {
-    if (action.type) {
-        
+    if (action.type === AUTH_USER) {
+        return {
+            ...state,
+            authenticated: true
+        };
+    }
+    if (action.type === UNAUTH_USER) {
+        return {
+            ...state,
+            authenticated: false
+        };
     }
     return state;
 }
