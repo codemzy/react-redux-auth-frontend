354a1b917c97fb451775435517bf401708a6b707
Emma J <codemzy@gmail.com>
2016-08-24 12:21:51 +0000

    Updated signinUser action creator to store the token to localStorage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+/* global localStorage */
 
 // action types
 import { AUTH_USER, UNAUTH_USER } from './types';
@@ -9,13 +10,13 @@ const ROOT_URL = 'https://node-jwt-auth-api-codemzy.c9users.io';
 export function signinUser({email, password}) {
     // with redux thunk we can return a function from our action creator
     return function(dispatch) {
-        
         // submit email and password to the api server
         axios.post(ROOT_URL + '/signin', { email, password })
             .then((response) => {
                 // if request is good update state to indicate user is authenticated (dispatch action)
                 dispatch({ type: AUTH_USER });
-                // save JWT token
+                // save JWT token to localStorage
+                localStorage.setItem('token', response.data.token);
                 // redirect to the route '/feature' using react router
                 browserHistory.push('/feature');
             })
