34cb8cbbcb11d4e94b120cf73c9f5d026695f093
Emma J <codemzy@gmail.com>
2016-08-24 13:28:27 +0000

    Updated Header component to renderAccountLinks based on authentication status

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import {Link, IndexLink} from 'react-router';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+    
+    _renderAccountLinks() {
+      if (this.props.authenticated) {
+        return (
+          <li className="nav-item">
+            <Link to="/signout" className="nav-link" activeClassName="active">Sign Out</Link>
+          </li>
+        );
+      } else {
+        return (
+          <div>
+            <li className="nav-item">
+              <Link to="/signin" className="nav-link" activeClassName="active">Sign In</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/register" className="nav-link" activeClassName="active">Register</Link>
+            </li>
+          </div>
+        );
+      }
+    }
+    
     render() {
         return (
             <nav className="navbar navbar-light bg-faded">
-              <a className="navbar-brand" href="#">Navbar</a>
+              <IndexLink to="/" className="navbar-brand" activeClassName="active">Home</IndexLink>
               <ul className="nav navbar-nav">
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">Sign In</a>
+                <li className="nav-item">
+                  {this._renderAccountLinks()}
                 </li>
               </ul>
             </nav>
@@ -15,4 +39,8 @@ class Header extends React.Component {
     }
 }
 
-export default Header;
+export default connect((state) => {
+  return {
+    authenticated: state.auth.authenticated
+  };
+})(Header);
