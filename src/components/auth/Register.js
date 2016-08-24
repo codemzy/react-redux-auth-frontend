d94728fd9b964d6090943eb83524099c6d4ae3b5
Emma J <codemzy@gmail.com>
2016-08-24 16:36:01 +0000

    Added form validation to Register component

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -10,17 +10,20 @@ class Register extends React.Component {
         return (
             <form>
                 <h1 className="page-title">Register.</h1>
-                <fieldset className="form-group">
+                <fieldset className={`form-group ${email.touched && email.invalid ? 'has-danger' : ''}`}>
                     <label>Email:</label>
                     <input type="text" className="form-control" {...email} />
+                    <div className="text-help form-control-label">{email.touched ? email.error : '' }</div>
                 </fieldset>
-                <fieldset className="form-group">
+                <fieldset className={`form-group ${password.touched && password.invalid ? 'has-danger' : ''}`}>
                     <label>Password:</label>
                     <input type="password" className="form-control" {...password} />
+                    <div className="text-help form-control-label">{password.touched ? password.error : '' }</div>
                 </fieldset>
-                <fieldset className="form-group">
+                <fieldset className={`form-group ${passwordConfirm.touched && passwordConfirm.invalid ? 'has-danger' : ''}`}>
                     <label>Confirm Password:</label>
                     <input type="password" className="form-control" {...passwordConfirm} />
+                    <div className="text-help form-control-label">{passwordConfirm.touched ? passwordConfirm.error : '' }</div>
                 </fieldset>
                 <button type="submit" className="btn btn-primary">Register</button>
             </form>
@@ -28,7 +31,25 @@ class Register extends React.Component {
     }
 }
 
+function validate(formProps) {
+    const ERRORS = {};
+    if (!formProps.email) {
+        ERRORS.email = 'Enter your email address';
+    }
+    if (!formProps.password) {
+        ERRORS.password = 'Enter a password';
+    }
+    if (!formProps.passwordConfirm) {
+        ERRORS.passwordConfirm = 'Confirm your password';
+    }
+    if (formProps.password !== formProps.passwordConfirm) {
+        ERRORS.passwordConfirm = 'Password does not match';
+    }
+    return ERRORS;
+}
+
 export default reduxForm({
     form: 'register',
-    fields: ['email', 'password', 'passwordConfirm']
+    fields: ['email', 'password', 'passwordConfirm'],
+    validate
 })(Register);
