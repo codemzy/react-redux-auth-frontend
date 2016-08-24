33fe5a1aa8d49f289b21269e368724df84abdb7d
Emma J <codemzy@gmail.com>
2016-08-24 13:39:55 +0000

    Added SignOut component and wired to router

---
+++ src/components/auth/SignOut.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+// actions
+import { signoutUser } from '../../actions/index.js';
+
+class SignOut extends React.Component {
+    
+    componentWillMount() {
+        this.props.dispatch(signoutUser());
+    }
+    
+    render() {
+        return (
+            <div className="jumbotron">
+                <h2>You have signed out. Bye for now...</h2>
+            </div>
+        );
+    }
+}
+
+export default connect()(SignOut);
--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 // components
 import App from './components/app';
 import SignIn from './components/auth/SignIn';
+import SignOut from './components/auth/SignOut';
 
 
 export default (
     <Route path='/' component={App}>
         <Route path="/signin" component={SignIn} />
+        <Route path="/signout" component={SignOut} />
     </Route>
 );
