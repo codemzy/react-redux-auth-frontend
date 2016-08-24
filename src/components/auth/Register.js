3afceea525e8872a9faeabf78c9f5f1bb3cd3bcb
Emma J <codemzy@gmail.com>
2016-08-24 14:13:28 +0000

    Scaffolded out form on Register component

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -6,12 +6,29 @@ import { reduxForm } from 'redux-form'
 
 class Register extends React.Component {
     render() {
+        const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
         return (
-            <div>
-                <h1>Register</h1>
-            </div>
+            <form>
+                <h1 className="page-title">Register.</h1>
+                <fieldset className="form-group">
+                    <label>Email:</label>
+                    <input type="text" className="form-control" {...email} />
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Password:</label>
+                    <input type="password" className="form-control" {...password} />
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Confirm Password:</label>
+                    <input type="password" className="form-control" {...passwordConfirm} />
+                </fieldset>
+                <button type="submit" className="btn btn-primary">Register</button>
+            </form>
         );
     }
 }
 
-export default Register;
+export default reduxForm({
+    form: 'register',
+    fields: ['email', 'password', 'passwordConfirm']
+})(Register);
