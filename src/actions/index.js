7e8f9a9df6233d55b32047de30c56a1d7480d69a
Emma J <codemzy@gmail.com>
2016-08-25 09:10:56 +0000

    Added signupUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -28,6 +28,15 @@ export function signinUser({email, password}) {
     };
 }
 
+export function signupUser({email, password}) {
+    return function(dispatch) {
+        axios.post(ROOT_URL + '/signup', { email, password });
+            // .then((response) => {
+                
+            // });
+    };
+}
+
 export function authError(error) {
     return {
         type: AUTH_ERROR,
