4ba201861c258fca1372c5729f7e9c43b31fa773
Emma J <codemzy@gmail.com>
2016-08-25 09:50:38 +0000

    Added Home component and wired up to router

---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <h1 className="page-title">Welcome to Authentication.</h1>
+    );
+};
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 
 // components
 import App from './components/app';
+import Home from './components/Home';
 import SignIn from './components/auth/SignIn';
 import SignOut from './components/auth/SignOut';
 import Register from './components/auth/Register';
@@ -13,7 +14,7 @@ import requireAuth from './components/auth/RequireAuth';
 
 export default (
     <Route path='/' component={App}>
-        <IndexRoute component={SignIn} />
+        <IndexRoute component={Home} />
         <Route path="/signin" component={SignIn} />
         <Route path="/signout" component={SignOut} />
         <Route path="/register" component={Register} />
