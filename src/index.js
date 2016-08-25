13c4b88b4aa41efe8570bf2b7245714be83a31a8
Emma J <codemzy@gmail.com>
2016-08-25 10:04:13 +0000

    Updated store to check for token in index.js to not log out users on refresh

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+/* global localStorage */
+
+// action type
+import { AUTH_USER } from './actions/types';
 
 // routes
 import { Router, browserHistory } from 'react-router';
@@ -12,9 +16,17 @@ import routes from './routes';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// if we have a token consider the user to be signed in
+if (token) {
+  // update the application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.app-wrap'));
